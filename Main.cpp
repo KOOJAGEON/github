385db4fa838e387660e02947c1456038816daa47
KOOJAGEON <ggplaygun@naver.com>
2022-12-26 13:52:42 +0900

    근의 공식

---
+++ Main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	double a, b, c = 0;
+	double d = 0;
+	double e = 0;
+
+	cin >> a;
+	cin >> b;
+	cin >> c;
+
+	e = (b * b) - (4 * a * c);
+	d = sqrt(e);
+
+	cout << (-b + d) / (2 * a) << endl;
+	cout << (-b - d) / (2 * a) << endl;
+
+	return 0;
+}
